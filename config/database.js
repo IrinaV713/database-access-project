829eda28ce6074693d4559a5ff1f6e49e43f2fde
IrinaV713 <76870532+IrinaV713@users.noreply.github.com>
2022-01-06 21:12:44 +0200

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    DATABASE_NAME: 'Your database name',
+    DATABASE_USERNAME: 'Your database username',
+    DATABASE_PASSWORD: 'Your password',
+    DATABASE_HOST: 'Database hostname',
+    DATABASE_DRIVER: 'mysql',
+}
