d25c5c0bf8a75a162480d8474182636e0cd53af7
IrinaV713 <76870532+IrinaV713@users.noreply.github.com>
2022-01-06 21:13:48 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
     DATABASE_NAME: 'Your database name',
     DATABASE_USERNAME: 'Your database username',
     DATABASE_PASSWORD: 'Your password',
-    DATABASE_HOST: 'Database hostname',
+    DATABASE_HOST: 'Your database hostname',
     DATABASE_DRIVER: 'mysql',
 }
