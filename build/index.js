5053edcaf9e4711dce3848e274d4f36e7e3987ed
IrinaV713 <76870532+IrinaV713@users.noreply.github.com>
2022-01-09 17:54:04 +0200

    Update index.js

--- build/index.js
+++ build/index.js
@@ -1,1 +1,20 @@
+const dbConfig = require('../../config/database');
+const fs = require('fs');
+const path = require('path');
+const mysql = require('mysql2');
 
+const sqlScript = fs.readFileSync(path.join(__dirname, '../../sql/db_sample.sql')).toString();
+
+const connection = mysql.connect({
+    host: dbConfig.DATABASE_HOST,
+    user: dbConfig.DATABASE_USERNAME,
+    password: dbConfig.DATABASE_PASSWORD,
+    database: dbConfig.DATABASE_NAME,
+    multipleStatements: true
+});
+
+connection.query(sqlScript, function (error) {
+    if (error) throw error;
+});
+
+process.exit(0);
