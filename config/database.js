c9400f1bed95bd9196847cb269c23c340494639c
IrinaV713 <76870532+IrinaV713@users.noreply.github.com>
2022-01-09 20:42:45 +0200

    Delete config directory

--- config/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    DATABASE_NAME: 'Your database name',
-    DATABASE_USERNAME: 'Your database username',
-    DATABASE_PASSWORD: 'Your password',
-    DATABASE_HOST: 'Your database hostname',
-    DATABASE_DRIVER: 'mysql',
-}
